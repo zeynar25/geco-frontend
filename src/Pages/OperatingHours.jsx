d28da4e3ec8413ba5b217a83c71b3d3ad53871c4
nate <ryrgnx@gmail.com>
2025-12-08 23:57:24 +0800

    Cards made

--- src/Pages/OperatingHours.jsx
+++ src/Pages/OperatingHours.jsx
@@ -1,10 +1,11 @@
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import HeaderCard from "../Components/HeaderCard.jsx";
 
 import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeftLong, faBuilding } from "@fortawesome/free-solid-svg-icons";
 
 function OperatingHours() {
   return (
@@ -24,6 +25,33 @@ function OperatingHours() {
             </h1>
             <p>Plan your visit with our detailed schedule information</p>
           </div>
+        </div>
+
+        <div className="grid grid-cols-3">
+          <HeaderCard
+            headerBg="bg-[#a9e2a3]"
+            headerColor="text-[#227B05]"
+            icon={<FontAwesomeIcon icon={faBuilding} />}
+            title="Regular Hours"
+            description="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            className="col-span-3 md:col-span-1"
+          />
+          <HeaderCard
+            headerBg="bg-[#a9e2a3]"
+            headerColor="text-[#227B05]"
+            icon={<FontAwesomeIcon icon={faBuilding} />}
+            title="Peak Hours"
+            description="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            className="col-span-3 md:col-span-1"
+          />
+          <HeaderCard
+            headerBg="bg-[#a9e2a3]"
+            headerColor="text-[#227B05]"
+            icon={<FontAwesomeIcon icon={faBuilding} />}
+            title="Quiet Hours"
+            description="lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            className="col-span-3 md:col-span-1"
+          />
         </div>
       </div>
       <Footer />
