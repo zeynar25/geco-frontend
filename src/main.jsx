2fc9fa5505253ab6d21704eb448e8f4f05c44512
nate <ryrgnx@gmail.com>
2025-12-08 18:16:06 +0800

    Tanstack setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Signin from "./Pages/Signin.jsx";
 import Book from "./Pages/Book.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -14,8 +15,12 @@ const router = createBrowserRouter([
   { path: "/book", element: <Book /> },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
