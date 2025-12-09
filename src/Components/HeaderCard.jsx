5fa4e0244b5ab49b20e4af4a03e714c602221bc0
nate <ryrgnx@gmail.com>
2025-12-09 23:59:25 +0800

    Added descriptionContent for custom description values; headerClass is the combination of headerBg, and headerColor

--- src/Components/HeaderCard.jsx
+++ src/Components/HeaderCard.jsx
@@ -1,22 +1,23 @@
 function AboutCard({
-  headerBg = "bg-white",
-  headerColor = "text-black",
+  headerClass = "bg-white text-black",
   icon,
   title,
   description,
+  descriptionContent,
   className,
 }) {
   return (
     <div
-      className={`rounded-xl border-0 shadow-sm overflow-hidden m-5 ${className}`}
+      className={`bg-white rounded-xl border-0 shadow-sm overflow-hidden m-5 ${className}`}
     >
-      <header
-        className={`${headerBg} ${headerColor} flex items-center px-10 py-5 gap-3`}
-      >
+      <header className={`${headerClass} flex items-center px-10 py-5 gap-3`}>
         <span className="text-lg lg:text-xl">{icon}</span>
         <h1 className="font-bold text-lg lg:text-xl">{title}</h1>
       </header>
-      <div className="px-10 py-5 flex items-center bg-white">{description}</div>
+
+      <div className="px-10 py-5 flex items-center bg-white">
+        {descriptionContent ? descriptionContent : description}
+      </div>
     </div>
   );
 }
