02454e394b8566a79e722c05551dd2e51dca1da5
nate <ryrgnx@gmail.com>
2025-12-09 23:38:41 +0800

    Responsive header text size

--- src/Components/HeaderCard.jsx
+++ src/Components/HeaderCard.jsx
@@ -13,8 +13,8 @@ function AboutCard({
       <header
         className={`${headerBg} ${headerColor} flex items-center px-10 py-5 gap-3`}
       >
-        <span className="text-xl">{icon}</span>
-        <h1 className="font-bold text-xl">{title}</h1>
+        <span className="text-lg lg:text-xl">{icon}</span>
+        <h1 className="font-bold text-lg lg:text-xl">{title}</h1>
       </header>
       <div className="px-10 py-5 flex items-center bg-white">{description}</div>
     </div>
