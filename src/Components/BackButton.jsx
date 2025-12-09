56e62e1b7e96d67eda47e7ef7fb510e92ca6acc5
nate <ryrgnx@gmail.com>
2025-12-09 23:23:19 +0800

    BackButton component implemented

---
+++ src/Components/BackButton.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+
+import { Link } from "react-router-dom";
+
+function BackButton(props) {
+  return (
+    <div className="flex items-center gap-3 mb-10">
+      <Link to={`${props.to}`}>
+        <FontAwesomeIcon
+          icon={faArrowLeftLong}
+          className="text-black border rounded-sm p-2 hover:text-green-600"
+        />
+      </Link>
+      <div className="flex flex-col">
+        <h1 className="font-bold text-xl text-[#227B05]">{props.title}</h1>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default BackButton;
