73e7e3783b55ef20b68f03fd313c91525d545589
nate <ryrgnx@gmail.com>
2025-12-11 00:03:18 +0800

    headerContent updated

--- src/Components/HeaderCard.jsx
+++ src/Components/HeaderCard.jsx
@@ -3,6 +3,7 @@ function AboutCard({
   icon,
   title,
   subtitle,
+  headerContent,
   descriptionClass,
   description,
   descriptionContent,
@@ -12,13 +13,17 @@ function AboutCard({
     <div
       className={`bg-white rounded-xl border-0 shadow-sm overflow-hidden flex flex-col h-full ${className}`}
     >
-      <header className={`flex px-10 py-5 gap-3 ${headerClass} `}>
-        <span className="text-lg lg:text-xl my-auto">{icon}</span>
-        <div className="my-auto">
-          <h1 className="font-bold text-lg lg:text-xl">{title}</h1>
-          <h6 className="font-bold text-xs lg:text-sm">{subtitle}</h6>
-        </div>
-      </header>
+      {headerContent ? (
+        headerContent
+      ) : (
+        <header className={`flex px-10 py-5 gap-3 ${headerClass} `}>
+          <span className="text-lg lg:text-xl my-auto">{icon}</span>
+          <div className="my-auto">
+            <h1 className="font-bold text-lg lg:text-xl">{title}</h1>
+            <h6 className="font-bold text-xs lg:text-sm">{subtitle}</h6>
+          </div>
+        </header>
+      )}
 
       <div className={`px-10 py-5 ${descriptionClass}`}>
         {descriptionContent ? descriptionContent : description}
