d0af222a857bab7495196cba04dec9a2bd564981
nate <ryrgnx@gmail.com>
2025-12-15 21:36:47 +0800

    BackButton and ParkCalendarButton implemented

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faCalendar,
   faCircleCheck,
   faEnvelope,
   faLocationDot,
@@ -14,6 +15,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { faCalendarCheck } from "@fortawesome/free-regular-svg-icons";
 import { useQuery } from "@tanstack/react-query";
+import BackButton from "../Components/BackButton";
 
 function isLoggedIn() {
   const token = localStorage.getItem("token");
@@ -33,6 +35,9 @@ function isLoggedIn() {
 function Account() {
   const [loggedIn] = useState(isLoggedIn());
   const navigate = useNavigate();
+
+  const location = useLocation();
+  const backTo = location.state?.from || "/";
 
   useEffect(() => {
     if (!loggedIn) {
@@ -69,7 +74,22 @@ function Account() {
   return (
     <>
       <Header />
-      <div>
+      <div className="bg-green-50 px-5 sm:px-10 md:px-15 lg:px-20 py-10">
+        <BackButton
+          to={backTo}
+          title="My Account"
+          description="Manage your account details and bookings."
+          extraButton={
+            <Link
+              to="/park-calendar"
+              state={{ from: location.pathname }}
+              className="bg-[#4D9C43] hover:bg-[#4D9C43]/95 text-[#FDDB3C] px-4 py-2 rounded-md flex items-center my-auto"
+            >
+              <FontAwesomeIcon icon={faCalendar} className="mr-2" />
+              <span>View Park Calendar</span>
+            </Link>
+          }
+        />
         {`${accountData?.detail?.firstName} ${accountData?.detail?.surname}`}
       </div>
       <Footer />
