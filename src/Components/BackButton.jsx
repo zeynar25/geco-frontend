9fc502a7683dcdd5165e153ce80b788ca8930fa0
nate <ryrgnx@gmail.com>
2025-12-15 21:36:25 +0800

    BackButton configured, added a possible extra button at the other end

--- src/Components/BackButton.jsx
+++ src/Components/BackButton.jsx
@@ -5,17 +5,20 @@ import { Link } from "react-router-dom";
 
 function BackButton(props) {
   return (
-    <div className="flex items-center gap-3 mb-5">
-      <Link to={`${props.to}`}>
-        <FontAwesomeIcon
-          icon={faArrowLeftLong}
-          className="text-black border rounded-sm p-2 hover:text-green-600"
-        />
-      </Link>
-      <div className="flex flex-col">
-        <h1 className="font-bold text-xl text-[#227B05]">{props.title}</h1>
-        <p>{props.description}</p>
+    <div className="flex mb-5 justify-between gap-5">
+      <div className="flex items-center gap-3">
+        <Link to={`${props.to}`}>
+          <FontAwesomeIcon
+            icon={faArrowLeftLong}
+            className="text-black border rounded-sm p-2 hover:text-green-600"
+          />
+        </Link>
+        <div className="flex flex-col">
+          <h1 className="font-bold text-xl text-[#227B05]">{props.title}</h1>
+          <p>{props.description}</p>
+        </div>
       </div>
+      {props.extraButton && props.extraButton}
     </div>
   );
 }
