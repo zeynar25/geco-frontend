26a030225ea7be55db3e5a42789e043017f7f87d
nate <ryrgnx@gmail.com>
2025-12-20 00:37:44 +0800

    admin dashboard stats implemented

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,9 +1,208 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import BackButton from "../Components/BackButton";
+
+import { useQuery } from "@tanstack/react-query";
+import { jwtDecode } from "jwt-decode";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faClock,
+  faMessage,
+  faPesoSign,
+} from "@fortawesome/free-solid-svg-icons";
+
+function isLoggedIn() {
+  const token = localStorage.getItem("token");
+  if (!token) return false;
+
+  try {
+    const decoded = jwtDecode(token);
+    if (decoded.exp && Date.now() < decoded.exp * 1000) {
+      return true;
+    }
+    return false;
+  } catch {
+    return false;
+  }
+}
+
 function AdminDashboard() {
+  const location = useLocation();
+  const backTo = location.state?.from || "/";
+  const navigate = useNavigate();
+
+  const [loggedIn] = useState(isLoggedIn());
+
+  useEffect(() => {
+    if (!loggedIn) {
+      alert("Please log in to book a visit.");
+      navigate("/signin");
+    }
+  }, [loggedIn, navigate]);
+
+  const {
+    data: accountData,
+    error: accountError,
+    isPending: accountPending,
+  } = useQuery({
+    queryKey: ["account"],
+    queryFn: async () => {
+      const token = localStorage.getItem("token");
+      const decoded = jwtDecode(token);
+      const account = await fetch(
+        `http://localhost:8080/account/${decoded.sub}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (!account.ok) {
+        const error = await account.json();
+        throw new Error(error?.error || "Getting account failed");
+      }
+      return await account.json();
+    },
+  });
+
+  if (accountError) {
+    alert("something went wrong in retrieving account");
+  }
+
+  if (accountData && accountData.role !== "ADMIN") {
+    alert("You do not have admin privileges to access this page.");
+    navigate("/");
+  }
+
+  const {
+    data: adminDashboardData,
+    error: adminDashboardError,
+    isPending: adminDashboardPending,
+  } = useQuery({
+    queryKey: ["dashboardStatistics"],
+    queryFn: async () => {
+      const token = localStorage.getItem("token");
+      const stats = await fetch("http://localhost:8080/dashboard", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!stats.ok) {
+        const error = await stats.json();
+        throw new Error(error?.error || "Getting dashboard statistics failed");
+      }
+      return await stats.json();
+    },
+  });
+
+  if (adminDashboardError) {
+    alert("something went wrong in retrieving dashboard statistics");
+  }
+
+  console.log("adminDashboardData:", adminDashboardData);
+
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      {/* Additional admin dashboard content goes here */}
-    </div>
+    <>
+      <Header />
+      <main className="bg-green-50 px-5 sm:px-10 md:px-15 lg:px-20 py-10 flex flex-col gap-5">
+        <BackButton
+          to={backTo}
+          title="Admin Dashboard"
+          description="Manage bookings, review feedbacks, and track park performance."
+        />
+
+        <div>
+          {adminDashboardPending ? (
+            <div className="flex flex-col items-center justify-center gap-4 h-100">
+              <div className="h-10 w-10 border-4 border-green-600 border-t-transparent rounded-full animate-spin" />
+              <p className="text-gray-600">Loading dashboard statistics...</p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-4 gap-5">
+              <div className="col-span-4 sm:col-span-2 lg:col-span-1 bg-white p-5 rounded shadow-lg">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col justify-between">
+                    <span className="font-semibold text-sm">
+                      This Month's Booking
+                    </span>
+                    <span className="font-bold text-lg">
+                      {adminDashboardData.monthlyBooking}
+                    </span>
+                  </div>
+                  <FontAwesomeIcon
+                    icon={faCalendar}
+                    className="text-xl bg-[#BAD0F8] text-[#222EDA] p-3 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-4 sm:col-span-2 lg:col-span-1 bg-white p-5 rounded shadow-lg">
+                <div className="flex items-center gap-2">
+                  <div className="flex flex-col justify-between">
+                    <span className="font-semibold text-sm">
+                      This Month's Revenue
+                    </span>
+                    <span className="font-bold text-lg">
+                      <FontAwesomeIcon icon={faPesoSign} />
+                      {adminDashboardData.monthlyRevenue}
+                    </span>
+                  </div>
+                  <FontAwesomeIcon
+                    icon={faPesoSign}
+                    className="text-xl bg-[#227B05]/20 text-[#227B05] p-3 rounded-md"
+                  />
+                </div>
+              </div>
+
+              <div className="col-span-4 sm:col-span-2 lg:col-span-1 bg-white p-5 rounded shadow-lg">
+                <div className="flex items-center gap-2 justify-between">
+                  <div className="flex flex-col">
+                    <span className="font-semibold text-sm">
+                      Pending Booking(s)
+                    </span>
+                    <span className="font-bold text-lg">
+                      {adminDashboardData.pendingBookings}
+                    </span>
+                  </div>
+                  <FontAwesomeIcon
+                    icon={faClock}
+                    className="text-xl bg-[#97750B]/20 text-[#97750B] p-3 rounded-md"
+                  />
+                </div>
+                <div className="text-sm text-gray-600 mt-3">
+                  Requires attention
+                </div>
+              </div>
+
+              <div className="col-span-4 sm:col-span-2 lg:col-span-1 bg-white p-5 rounded shadow-lg">
+                <div className="flex items-center gap-2 justify-between">
+                  <div className="flex flex-col">
+                    <span className="font-semibold text-sm">
+                      New Feedback(s)
+                    </span>
+                    <span className="font-bold text-lg">
+                      {adminDashboardData.unreadFeedback}
+                    </span>
+                  </div>
+                  <FontAwesomeIcon
+                    icon={faMessage}
+                    className="text-xl bg-[#A86CCB]/20 text-[#A86CCB] p-3 rounded-md"
+                  />
+                </div>
+                <div className="text-sm text-gray-600 mt-3">
+                  Unread responses
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }
 
