9f6583d8c29ffcd36a74390e9f18e27b1d77d16f
nate <ryrgnx@gmail.com>
2025-12-25 12:50:29 +0800

    show bookings by filter implemented

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -13,7 +13,10 @@ import {
   faClock,
   faMessage,
   faPesoSign,
+  faCheckCircle,
+  faCircleXmark,
 } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarCheck } from "@fortawesome/free-regular-svg-icons";
 
 function isLoggedIn() {
   const token = localStorage.getItem("token");
@@ -36,6 +39,17 @@ function AdminDashboard() {
   const navigate = useNavigate();
 
   const [loggedIn] = useState(isLoggedIn());
+
+  const [bookingIn, setBookingIn] = useState(true);
+  const [financesIn, setFinancesIn] = useState(false);
+  const [trendsIn, setTrendsIn] = useState(false);
+  const [bookingFilter, setBookingFilter] = useState("ALL");
+  const [paymentFilter, setPaymentFilter] = useState("ALL");
+  const [paymentMethodFilter, setPaymentMethodFilter] = useState("ALL");
+  const [bookingPage, setBookingPage] = useState(0);
+  const [feedbacks] = useState([]);
+  const openFeedbackModal = () => {};
+  const openNewFeedbackModal = () => {};
 
   useEffect(() => {
     if (!loggedIn) {
@@ -103,7 +117,60 @@ function AdminDashboard() {
     alert("something went wrong in retrieving dashboard statistics");
   }
 
-  console.log("adminDashboardData:", adminDashboardData);
+  const {
+    data: bookingData,
+    error: bookingError,
+    isPending: bookingPending,
+  } = useQuery({
+    queryKey: [
+      "bookings",
+      bookingPage,
+      bookingFilter,
+      paymentFilter,
+      paymentMethodFilter,
+    ],
+    enabled: bookingIn,
+    queryFn: async () => {
+      const token = localStorage.getItem("token");
+      const baseUrl = "http://localhost:8080/booking";
+
+      const params = new URLSearchParams();
+      params.append("page", bookingPage.toString());
+      params.append("size", "5");
+      if (bookingFilter !== "ALL") {
+        params.append("bookingStatus", bookingFilter.toUpperCase());
+      }
+      if (paymentFilter !== "ALL") {
+        params.append("paymentStatus", paymentFilter.toUpperCase());
+      }
+      if (paymentMethodFilter !== "ALL") {
+        params.append("paymentMethod", paymentMethodFilter.toUpperCase());
+      }
+
+      const queryString = params.toString();
+      const endpoint = `${baseUrl}?${queryString}`;
+
+      console.log("Fetching bookings from:", endpoint);
+
+      const bookings = await fetch(endpoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (!bookings.ok) {
+        const error = await bookings.json();
+        throw new Error(error?.error || "Getting bookings failed");
+      }
+      return await bookings.json();
+    },
+  });
+
+  if (bookingError) {
+    alert("something went wrong in retrieving bookings");
+  }
+
+  const bookings = bookingData?.content ?? [];
+  const totalBookingPages = bookingData?.totalPages ?? 0;
 
   return (
     <>
@@ -162,7 +229,8 @@ function AdminDashboard() {
                 <div className="flex items-center gap-2 justify-between">
                   <div className="flex flex-col">
                     <span className="font-semibold text-sm">
-                      Pending Booking(s)
+                      Pending Booking{" "}
+                      {adminDashboardData.pendingBookings > 1 ? "s" : ""}
                     </span>
                     <span className="font-bold text-lg">
                       {adminDashboardData.pendingBookings}
@@ -173,16 +241,20 @@ function AdminDashboard() {
                     className="text-xl bg-[#97750B]/20 text-[#97750B] p-3 rounded-md"
                   />
                 </div>
-                <div className="text-sm text-gray-600 mt-3">
-                  Requires attention
-                </div>
+
+                {adminDashboardData.pendingBookings > 0 && (
+                  <div className="text-sm text-gray-600 mt-3">
+                    Requires attention
+                  </div>
+                )}
               </div>
 
               <div className="col-span-4 sm:col-span-2 lg:col-span-1 bg-white p-5 rounded shadow-lg">
                 <div className="flex items-center gap-2 justify-between">
                   <div className="flex flex-col">
                     <span className="font-semibold text-sm">
-                      New Feedback(s)
+                      New Feedback
+                      {adminDashboardData.unreadFeedback > 1 ? "s" : ""}
                     </span>
                     <span className="font-bold text-lg">
                       {adminDashboardData.unreadFeedback}
@@ -193,12 +265,196 @@ function AdminDashboard() {
                     className="text-xl bg-[#A86CCB]/20 text-[#A86CCB] p-3 rounded-md"
                   />
                 </div>
-                <div className="text-sm text-gray-600 mt-3">
-                  Unread responses
+
+                {adminDashboardData.unreadFeedback > 0 && (
+                  <div className="text-sm text-gray-600 mt-3">
+                    Unread responses
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="bg-white shadow-xl flex justify-around">
+          <button
+            type="button"
+            className={`px-4 py-2 rounded-full text-sm font-medium transition-colors ${
+              bookingIn ? "text-[#227B05]" : "text-black"
+            }`}
+            onClick={() => {
+              setBookingIn(true);
+              setFinancesIn(false);
+              setTrendsIn(false);
+            }}
+          >
+            Bookings
+          </button>{" "}
+          <button
+            type="button"
+            className={`px-4 py-2 rounded-full text-sm font-medium transition-colors ${
+              financesIn ? "text-[#227B05]" : "text-black"
+            }`}
+            onClick={() => {
+              setBookingIn(false);
+              setFinancesIn(true);
+              setTrendsIn(false);
+            }}
+          >
+            Finances
+          </button>{" "}
+          <button
+            type="button"
+            className={`px-4 py-2 rounded-full text-sm font-medium transition-colors ${
+              trendsIn ? "text-[#227B05]" : "text-black"
+            }`}
+            onClick={() => {
+              setBookingIn(false);
+              setFinancesIn(false);
+              setTrendsIn(true);
+            }}
+          >
+            Trends
+          </button>
+        </div>
+
+        <div>
+          {bookingIn && (
+            <div className="rounded-lg overflow-hidden shadow-xl">
+              <div className="text-white bg-[#48BF56] px-4 py-2">
+                <FontAwesomeIcon
+                  icon={faCalendarCheck}
+                  className="mr-2 text-lg"
+                />
+                <span>Booking Management</span>
+              </div>
+              <div>
+                <form className="flex justify-around mt-3">
+                  <div>
+                    <span>Booking Status:</span>
+                    <select
+                      value={bookingFilter}
+                      onChange={(e) => setBookingFilter(e.target.value)}
+                      className="ml-2 border border-[#227B05]"
+                    >
+                      <option value="ALL">All</option>
+                      <option value="PENDING">Pending</option>
+                      <option value="CANCELLED">Cancelled</option>
+                      <option value="APPROVED">Approved</option>
+                      <option value="REJECTED">Rejected</option>
+                      <option value="COMPLETED">Completed</option>
+                    </select>
+                  </div>
+
+                  <div>
+                    <span>Payment Status:</span>
+                    <select
+                      value={paymentFilter}
+                      onChange={(e) => setPaymentFilter(e.target.value)}
+                      className="ml-2 border border-[#227B05]"
+                    >
+                      <option value="ALL">All</option>
+                      <option value="UNPAID">Unpaid</option>
+                      <option value="PAYMENT_VERIFICATION">
+                        Payment Verified
+                      </option>
+                      <option value="VERIFIED">Verified</option>
+                      <option value="REJECTED">Rejected</option>
+                      <option value="REFUNDED">Refunded</option>
+                    </select>
+                  </div>
+
+                  <div>
+                    <span>Payment Method:</span>
+                    <select
+                      value={paymentMethodFilter}
+                      onChange={(e) => setPaymentMethodFilter(e.target.value)}
+                      className="ml-2 border border-[#227B05]"
+                    >
+                      <option value="ALL">All</option>
+                      <option value="PARK">On-park</option>
+                      <option value="ONLINE">Online</option>
+                    </select>
+                  </div>
+                </form>
+                <div className="p-4 flex flex-col gap-5">
+                  {bookingPending && (
+                    <p className="text-gray-600 text-sm">Loading bookings...</p>
+                  )}
+
+                  {!bookingPending &&
+                    bookingData &&
+                    bookings.map((booking) => (
+                      <div
+                        key={booking.bookingId}
+                        className="px-5 md:px-10 py-3 border border-[#227B05] rounded-lg"
+                      >
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-5 justify-between">
+                          <div className="col-span-2 xs:col-span-1 flex flex-col justify-center items-center">
+                            <div className="text-[#227B05] font-semibold text-lg">
+                              Visit on:{" "}
+                              {new Date(booking.visitDate).toLocaleDateString()}
+                            </div>
+                            <div>
+                              {booking.groupSize} visitor(s) at{" "}
+                              {booking.visitTime}
+                            </div>
+                          </div>
+                          <div className="col-span-2 xs:col-span-1 flex flex-col justify-center items-center text-center">
+                            <div>{booking.tourPackage.name}</div>
+                            <div>
+                              <FontAwesomeIcon icon={faPesoSign} />
+                              {booking.totalPrice}
+                            </div>
+                          </div>
+                          <div className="col-span-2 md:col-span-1 flex justify-center items-center">
+                            {/* Payment Method: {booking.paymentMethod} */}
+                            <div
+                              className={
+                                "font-semibold px-3 py-2 rounded-full " +
+                                (booking.bookingStatus === "PENDING"
+                                  ? "bg-[#FDDB3C]"
+                                  : booking.bookingStatus === "CANCELLED"
+                                  ? "bg-[#E32726]/70"
+                                  : booking.bookingStatus === "APPROVED"
+                                  ? "bg-[#BAD0F8]/90"
+                                  : booking.bookingStatus === "REJECTED"
+                                  ? "bg-[#E32726]/70"
+                                  : booking.bookingStatus === "COMPLETED"
+                                  ? "bg-[#A86CCB]"
+                                  : "")
+                              }
+                            >
+                              {booking.bookingStatus === "PENDING" ? (
+                                <FontAwesomeIcon
+                                  icon={faClock}
+                                  className="mr-2"
+                                />
+                              ) : booking.bookingStatus === "CANCELLED" ||
+                                booking.bookingStatus === "REJECTED" ? (
+                                <FontAwesomeIcon
+                                  icon={faCircleXmark}
+                                  className="mr-2"
+                                />
+                              ) : booking.bookingStatus === "APPROVED" ||
+                                booking.bookingStatus === "COMPLETED" ? (
+                                <FontAwesomeIcon
+                                  icon={faCheckCircle}
+                                  className="mr-2 text-xl"
+                                />
+                              ) : null}
+                              {booking.bookingStatus}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
                 </div>
               </div>
             </div>
           )}
+
+          {financesIn && <div></div>}
         </div>
       </main>
       <Footer />
